7fabe84739c622ba4dc2b89b926a971f49fdc6b2
Joan Ciprià <joancipria@protonmail.com>
2022-01-25 12:48:36 +0100

    Add leflet popups

---
+++ src/components/OverlapPopup/OverlapPopup.ts
@@ -0,0 +1,39 @@
+import { Popup } from "leaflet";
+import { i18n } from "@/main";
+import { Overlap } from "../../core/Overlap";
+
+export class OverlapPopup extends Popup {
+    overlap: Overlap;
+
+    constructor(overlap: Overlap) {
+        super();
+        this.overlap = overlap;
+        this.setLoading();
+    }
+
+    setStats() {
+        super.setContent(`
+        <table class="table">
+            <tbody>
+                <tr>
+                <th scope="row">${i18n.COVERED_POPULATION}:</th>
+                <td>${this.overlap.population || i18n.NOT_AVAILABLE_ERROR}</td>
+                </tr>
+            </tbody>
+        </table>    
+        `);
+    }
+
+    setLoading() {
+        super.setContent(`
+        <table class="table">
+            <tbody>
+                <tr>
+                <th scope="row">${i18n.COVERED_POPULATION}:</th>
+                <td>${i18n.LOADING}</td>
+                </tr>
+            </tbody>
+        </table>    
+        `);
+    }
+}
---
+++ src/components/VehiclePopup/VehiclePopup.ts
@@ -0,0 +1,55 @@
+import { Popup } from "leaflet";
+import Vehicle from "@/core/Vehicle";
+import { i18n } from "@/main";
+
+export class VehiclePopup extends Popup {
+    vehicle: Vehicle;
+
+    constructor(vehicle: Vehicle) {
+        super();
+        this.vehicle = vehicle;
+        this.setLoading();
+    }
+
+    setStats() {
+        super.setContent(`
+        <table class="table">
+            <tbody>
+                <tr>
+                <th scope="row">${i18n.NAME}:</th>
+                <td>${this.vehicle.name}</td>
+                </tr>
+                <tr>
+                <th scope="row">${i18n.AVAILABILITY}:</th>
+                <td>${this.vehicle.availability}</td>
+                </tr>
+                <tr>
+                <th scope="row">${i18n.COVERED_POPULATION}:</th>
+                <td>${this.vehicle.population || i18n.NOT_AVAILABLE_ERROR}</td>
+                </tr>
+            </tbody>
+        </table>    
+        `);
+    }
+
+    setLoading() {
+        super.setContent(`
+        <table class="table">
+            <tbody>
+                <tr>
+                <th scope="row">${i18n.NAME}:</th>
+                <td>${this.vehicle.name}</td>
+                </tr>
+                <tr>
+                <th scope="row">${i18n.AVAILABILITY}:</th>
+                <td>${this.vehicle.availability}</td>
+                </tr>
+                <tr>
+                <th scope="row">${i18n.COVERED_POPULATION}:</th>
+                <td>${i18n.LOADING} ...</td>
+                </tr>
+            </tbody>
+        </table>    
+        `);
+    }
+}
