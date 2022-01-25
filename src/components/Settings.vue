217fa0136f96b26b6df754aa12d90c8ac95c41bd
Joan Ciprià <joancipria@protonmail.com>
2022-01-25 20:07:32 +0100

    Beautify code

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -3,30 +3,30 @@
     <div class="modal-background"></div>
     <div class="modal-card">
       <header class="modal-card-head">
-        <p class="modal-card-title">{{i18n.SETTINGS}}</p>
+        <p class="modal-card-title">{{ i18n.SETTINGS }}</p>
         <button @click="hide" class="delete" aria-label="close"></button>
       </header>
       <section class="modal-card-body">
         <div class="field">
-          <label class="label">{{i18n.LANGUAGE}}</label>
+          <label class="label">{{ i18n.LANGUAGE }}</label>
           <div class="control">
             <div class="select">
               <select>
-                <option>{{i18n.ENGLISH}}</option>
-                <option>{{i18n.SPANISH}}</option>
-                <option>{{i18n.CATALAN}}</option>
+                <option>{{ i18n.SPANISH }}</option>
+                <option>{{ i18n.CATALAN }}</option>
+                <option>{{ i18n.ENGLISH }}</option>
               </select>
             </div>
           </div>
         </div>
         <div class="field mt-5">
-          <label class="label">SAMU {{i18n.ISOCHRONE_TIME}}</label>
+          <label class="label">SAMU {{ i18n.ISOCHRONE_TIME }}</label>
           <div class="control">
             <TimeController :group="VehicleType.SAMU"></TimeController>
           </div>
         </div>
         <div class="field mt-5">
-          <label class="label">SVB {{i18n.ISOCHRONE_TIME}}</label>
+          <label class="label">SVB {{ i18n.ISOCHRONE_TIME }}</label>
           <div class="control">
             <TimeController :group="VehicleType.SVB"></TimeController>
           </div>
@@ -34,7 +34,9 @@
       </section>
       <footer class="modal-card-foot">
         <div class="buttons is-right">
-          <button @click="hide" class="button is-success">{{i18n.SAVE_CHANGES}}</button>
+          <button @click="hide" class="button is-success">
+            {{ i18n.SAVE_CHANGES }}
+          </button>
         </div>
       </footer>
     </div>
