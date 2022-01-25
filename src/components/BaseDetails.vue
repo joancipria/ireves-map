abff4138d9b077fedebf0ce777bd28b029c69df7
Joan Ciprià <joancipria@protonmail.com>
2022-01-25 12:53:12 +0100

    Add base details component

---
+++ src/components/BaseDetails.vue
@@ -0,0 +1,138 @@
+<template>
+  <div v-if="base" :class="{ open: base && showDetails }" class="box details">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Base details</p>
+      <button @click="hide" class="delete" aria-label="close"></button>
+    </header>
+    <div class="content mt-5">
+      <div class="columns">
+        <div class="colum">
+          <img src="@/assets/base.svg" alt="" />
+        </div>
+        <div class="column">
+          <div class="column">
+            <p>{{ base.name }}</p>
+          </div>
+          <div class="column">
+            <p>
+              <i class="fas fa-map-marker-alt has-text-danger"></i>
+              {{ base.address }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <!-- <div class="column">
+          <h1 class="title is-5">Covered population</h1>
+          <i class="fas fa-users"></i>
+        </div> -->
+      </div>
+      <div class="columns">
+        <div class="column">
+          <br />
+          <h1 class="title is-5">Available ambulances</h1>
+          <div class="list has-visible-pointer-controls">
+            <div
+              v-for="vehicle of base.vehicles"
+              :key="vehicle.id"
+              class="list-item"
+            >
+              <div class="list-item-image">
+                <figure class="image is-64x64">
+                  <img
+                    v-if="vehicle.type == 'SAMU'"
+                    class="is-rounded"
+                    src="@/assets/samu.png"
+                  />
+                  <img v-else class="is-rounded" src="@/assets/svb.png" />
+                </figure>
+              </div>
+
+              <div class="list-item-content">
+                <div class="list-item-title">{{ vehicle.name }}</div>
+                <div class="list-item-description">
+                  <div class="tag is-rounded">
+                    {{ vehicle.type }}
+                  </div>
+
+                  <div class="tag is-rounded">
+                    {{ vehicle.availability }}
+                  </div>
+                </div>
+              </div>
+              <div class="list-item-controls">
+                <div class="buttons is-right">
+                  <button @click="extractVehicle(vehicle)" class="button">
+                    <span class="icon is-small">
+                      <i class="fas fa-sign-out-alt"></i>
+                    </span>
+                    <span>Extract</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Base from "@/core/Base";
+import Vehicle from "@/core/Vehicle";
+
+@Options({
+  props: {
+    base: Base,
+  },
+})
+export default class BaseDetails extends Vue {
+  showDetails: boolean = false;
+  base!: Base;
+  selectedVehicle: number;
+
+  show() {
+    this.showDetails = true;
+  }
+
+  hide() {
+    this.showDetails = false;
+  }
+
+  extractVehicle(vehicle: Vehicle) {
+    this.base.extractVehicle(vehicle);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.details {
+  z-index: 10;
+  position: fixed;
+  right: -30%;
+  height: 100%;
+  width: 30%;
+  -webkit-box-shadow: -3px 0px 5px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: -3px 0px 5px 0px rgba(0, 0, 0, 0.2);
+  transition: all ease-in-out 0.3s;
+
+  header {
+    background: #fff;
+    padding: 10px;
+  }
+}
+.open {
+  transform: translate3d(-100%, 0, 0);
+  animation-timing-function: 1s ease-in;
+}
+.box {
+  border-top: 1px solid #dbdbdb;
+  border-radius: 0px;
+}
+.tag {
+  margin-right: 2px;
+}
+</style>
