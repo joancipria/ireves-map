7d7eb75fe5e366b2c56a2faa5818a8722dcf35a0
Joan Ciprià <joancipria@protonmail.com>
2022-01-25 13:36:48 +0100

    Add settings component

---
+++ src/components/Settings.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="modal" :class="{ 'is-active': modalActive }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">{{i18n.SETTINGS}}</p>
+        <button @click="hide" class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <div class="field">
+          <label class="label">{{i18n.LANGUAGE}}</label>
+          <div class="control">
+            <div class="select">
+              <select>
+                <option>{{i18n.ENGLISH}}</option>
+                <option>{{i18n.SPANISH}}</option>
+                <option>{{i18n.CATALAN}}</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="field mt-5">
+          <label class="label">SAMU {{i18n.ISOCHRONE_TIME}}</label>
+          <div class="control">
+            <TimeController :group="'SAMU'"></TimeController>
+          </div>
+        </div>
+        <div class="field mt-5">
+          <label class="label">SVB {{i18n.ISOCHRONE_TIME}}</label>
+          <div class="control">
+            <TimeController :group="'SVB'"></TimeController>
+          </div>
+        </div>
+      </section>
+      <footer class="modal-card-foot">
+        <div class="buttons is-right">
+          <button @click="hide" class="button is-success">{{i18n.SAVE_CHANGES}}</button>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import TimeController from "@/components/TimeController.vue";
+import { i18n } from "@/main";
+
+@Options({
+  components: {
+    TimeController,
+  },
+  data() {
+    return {
+      i18n: i18n,
+    };
+  },
+  computed: {},
+})
+export default class Settings extends Vue {
+  modalActive: boolean = false;
+
+  show() {
+    this.modalActive = true;
+  }
+
+  hide() {
+    this.modalActive = false;
+  }
+}
+</script>
+<style scoped lang="scss">
+.box {
+  height: 50vh;
+}
+.modal-background {
+  background-color: rgba(10, 10, 10, 0.6);
+}
+</style>
