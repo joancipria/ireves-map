c8820260a41297c1015bb64165bf7e91c39f29a8
Joan Ciprià <joancipria@protonmail.com>
2022-01-25 19:46:22 +0100

    Add VehicleColor

--- src/core/VehicleProperties.ts
+++ src/core/VehicleProperties.ts
@@ -1,9 +1,14 @@
 export enum VehicleType {
-    SAMU = "SAMU",
-    SVB = "SVB"
+    SAMU = 'SAMU',
+    SVB = 'SVB'
 }
 
 export enum VehicleTime {
     SAMU = 10,
     SVB = 15
+}
+
+export enum VehicleColor{
+    SAMU = '#e61212',
+    SVB = '#129fe6'
 }
