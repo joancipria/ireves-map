bbc7d6d48012fdafe2353a77451e451d58c54ee5
Joan Ciprià <joancipria@protonmail.com>
2022-02-08 18:33:10 +0100

    Implement target layer

--- src/components/LeafletMarker/LeafletMarker.ts
+++ src/components/LeafletMarker/LeafletMarker.ts
@@ -1,6 +1,6 @@
 
 import CONFIG from "@/config";
-import { Marker, LatLng, LatLngExpression, DivIcon } from "leaflet";
+import { Marker, LatLng, LatLngExpression, DivIcon, LayerGroup } from "leaflet";
 import './LeafletMarker.css';
 import { leafletMap } from "@/main";
 import { MapEntity } from "@/core/MapEntity"
@@ -9,7 +9,7 @@ export class LeafletMarker extends Marker {
 
     added: boolean;
 
-    constructor(entity: MapEntity, position: LatLng | LatLngExpression, draggable: boolean, customClass?: string, addToMap?: boolean) {
+    constructor(entity: MapEntity, position: LatLng | LatLngExpression, draggable: boolean, customClass?: string, addToMap?: boolean, targetLayer?: LayerGroup) {
 
         const icon = new DivIcon({
             html: `<img class="marker ${customClass}" src="${require(`@/assets/markers/${entity.toLowerCase()}_marker.png`)}">`,
@@ -22,14 +22,18 @@ export class LeafletMarker extends Marker {
             draggable: draggable
         });
         if (addToMap) {
-            this.addToMap();
+            this.addToMap(targetLayer);
         }
         this.added = false;
     }
 
-    addToMap() {
+    addToMap(targetLayer?: LayerGroup) {
         if (!this.added) {
-            super.addTo(leafletMap.map);
+            if (targetLayer) {
+                super.addTo(targetLayer)
+            } else {
+                super.addTo(leafletMap.map);
+            }
             this.added = true;
         }
     }
