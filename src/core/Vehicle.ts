bb52285472e429f6e778852a68bdee80df9ff4d9
Joan Ciprià <joancipria@protonmail.com>
2022-02-09 19:10:11 +0100

    Implement global overlap

--- src/core/Vehicle.ts
+++ src/core/Vehicle.ts
@@ -1,14 +1,13 @@
 import { Overlap } from "@/core/Overlap";
-import { LatLng, GeoJSON, LeafletEvent } from "leaflet";
-import { Feature } from "@turf/turf";
+import { LatLng, GeoJSON, LeafletEvent, LayerGroup } from "leaflet";
+import { Feature, MultiPolygon, Polygon } from "@turf/turf";
 import { LeafletMarker } from "@/components/LeafletMarker/LeafletMarker";
 import { openRoute } from "@/services/openroute.service";
 import { worldPop } from "@/services/worldpop.service";
-import { vehicles, vehicleOverlaps, leafletMap, eventEmitter } from "@/main";
+import { vehicles, vehicleOverlaps, eventEmitter, layers, globalOverlap } from "@/main";
 import { utils } from "./Utils";
-import { VehicleType, VehicleTime, VehicleColor } from "@/core/VehicleProperties";
+import { VehicleType, VehicleTime, VehicleColor, VehicleAvailability } from "@/core/VehicleProperties";
 import { MapEntity } from "@/core/MapEntity"
-
 import { VehiclePopup } from "@/components/VehiclePopup/VehiclePopup";
 
 // TODO: Implementar tipo de vehículo (bici, coche etc) y en función de eso, que openroute haga la petición correcta
@@ -18,52 +17,53 @@ export default class Vehicle {
     id: number;
     name: string;
     position: LatLng;
-    availability: string;
-    description: string;
+    availability: VehicleAvailability;
     type: string;
-    // subtype: string;
-    // tag: string;
 
     // --- Isochrone ---
+    active: boolean = false;
     time: number;
-    isochroneLayer: GeoJSON; // Leaflet isochrone layer
-    polygon: Feature; // Feature polygon from openRoute
-    overlaps: Overlap[];
-    population: number;
+    isochroneLayer: GeoJSON = null; // Leaflet isochrone layer
+    polygon: Feature<Polygon | MultiPolygon> = null; // Feature polygon from openRoute
+    population: number = null;
     color: string = '#e61212';
     marker: LeafletMarker;
     popup: VehiclePopup;
+    targetLayer: LayerGroup;
 
-    constructor(id: number, name: string, lat: number = 0, lng: number = 0, type: VehicleType, availability: string = '12 AM', description: string = '') {
+    constructor(id: number, name: string, lat: number = 0, lng: number = 0, type: VehicleType, availability: VehicleAvailability = VehicleAvailability["24H"]) {
+        // Set basic info
         this.id = id;
         this.name = name;
         this.position = new LatLng(lat, lng);
         this.availability = availability;
-        this.description = description;
         this.type = type;
-
-        this.isochroneLayer = null;
-        this.polygon = null;
-        this.overlaps = [];
-        this.population = null;
         this.time = VehicleTime[this.type]
+
+        // Set target layer
+        if (this.availability == VehicleAvailability["12H"]) {
+            this.targetLayer = layers.availability.halfDay;
+        }
+
+        if (this.availability == VehicleAvailability["12H N"]) {
+            this.targetLayer = layers.availability.halfDayNight;
+        }
+
+        if (this.availability == VehicleAvailability["24H"]) {
+            this.targetLayer = layers.availability.allDay;
+        }
 
         // Set color based on vehicle type
         this.color = VehicleColor[this.type]; // Color azul en hex
+
+        // Marker & Popup
         this.marker = new LeafletMarker(MapEntity[this.type], this.position, true, '');
+        this.popup = new VehiclePopup(this);
+        this.marker.bindPopup(this.popup);
 
-
-        // --- Define marker events ---
-        // On drag start, hide isochroneLayer and overlaps
-        this.marker.on('dragstart', () => {
-            this.toggleIsochroneLayer(false);
-
-            this.isochroneLayer = null;
-            this.polygon = null;
-            this.population = null;
-
-            this.clearOverlaps();
-        })
+        // --- Events ---
+        // On drag start, clear all vehicle data
+        this.marker.on('dragstart', this.deactivate.bind(this))
 
         // On drag ends, update  position
         this.marker.on('dragend', (event: LeafletEvent) => {
@@ -71,57 +71,74 @@ export default class Vehicle {
         })
 
         // On click
-        this.marker.on('click', async () => {
-            // Check if isochroneLayer has been generated
-            if (this.isochroneLayer) {
-                this.getPopulation(); // Then get population
-            } else {
-                await this.updateIsochrone()
-                this.getPopulation();
-            }
-        })
-
-        this.popup = new VehiclePopup(this);
-        this.marker.bindPopup(this.popup);
-
+        this.marker.on('click', this.activate.bind(this));
 
         // On time controller change
-        eventEmitter.on('timeChange', async (time, type) => {
-            if (type == this.type) {
-                console.log("tiempo actualizado")
-                // Update time
-                this.time = time;
-
-                // Clear overlaps
-                this.clearOverlaps();
-
-                // Update isochrone if there's a polygon
-                if (this.polygon) {
-                    await this.updateIsochrone();
-                    this.getPopulation();
-                }
-            }
-        });
+        eventEmitter.on('timeChange', this.onTimeChange);
     }
 
-    // TODO: Rename function
-    async onClick(checkOverlap: boolean = true) {
-        // Check if isochroneLayer has been generated
-        if (this.isochroneLayer) {
-            this.getPopulation(); // Then get population
-        } else {
-            await this.updateIsochrone(checkOverlap)
+    async activate() {
+        if (!this.active) {
+            console.info("Vehicle activated", this);
+
+            // Activate vehicle
+            this.active = true;
+
+            // Hide isochroneLayer
+            this.toggleIsochrone(false);
+
+            // Show loading in popup
+            this.popup.setLoading();
+
+            // Generate (new) isochrone
+            await this.generateIsochrone();
+
+            // Show isochroneLayer
+            this.toggleIsochrone(true);
+
+            // Check overlaps
+            this.checkOverlap();
+
+            // Get pop
             this.getPopulation();
         }
     }
 
-    async updateIsochrone(checkOverlap: boolean = true) {
-        // Hide isochroneLayer
-        this.toggleIsochroneLayer(false);
+    deactivate() {
+        if (this.active) {
+            console.info("Deactivate vehicle", this);
 
-        // Show loading in popup
-        this.popup.setLoading();
+            // Hide isochrone
+            this.toggleIsochrone(false);
 
+            if (globalOverlap.overlap && this.polygon) { // TODO: Sólo si hay overlap
+                this.clearOverlaps();
+            }
+
+            this.isochroneLayer = null;
+            this.polygon = null;
+            this.population = null;
+
+            // Deactivate vehicle
+            this.active = false;
+        }
+    }
+
+    async onTimeChange(time, type) {
+        if (type == this.type) {
+            // Update time
+            this.time = time;
+
+            // Deactivate vehicle
+            this.deactivate();
+
+            // Update isochrone if is active
+            this.activate();
+        }
+
+    }
+
+    async generateIsochrone() {
         // Get new isochrone
         const data = await openRoute.getVehicleIsochron(this);
 
@@ -138,43 +155,28 @@ export default class Vehicle {
                     color: this.color
                 }
             });
-
-            // Show isochroneLayer
-            this.toggleIsochroneLayer(true);
-        }
-
-
-        if (checkOverlap) {
-            // Check overlaps
-            this.checkOverlap();
         }
     }
 
-    updatePosition(coord: LatLng) {
-        this.position.lat = coord.lat;
-        this.position.lng = coord.lng;
-        this.marker.setLatLng(coord);
-    }
-
-    toggleIsochroneLayer(visibility = false) {
+    toggleIsochrone(visibility = false) {
         if (!this.isochroneLayer) return;
 
         if (!visibility) {
             if (this.isochroneLayer) {
-                leafletMap.map.removeLayer(this.isochroneLayer);
+                this.targetLayer.removeLayer(this.isochroneLayer)
             }
             return;
         }
 
         if (this.isochroneLayer) {
-            leafletMap.map.addLayer(this.isochroneLayer);
+            this.isochroneLayer.addTo(this.targetLayer);
         }
     }
 
     async getPopulation() {
         if (!this.isochroneLayer) return;
 
-        // Get populaton
+        // Get population
         const data = await worldPop.getPopulation(this.polygon.geometry);
 
         if (data.error) {
@@ -197,31 +199,73 @@ export default class Vehicle {
 
                 // If there's overlap
                 if (overlap) {
+                    console.info("Overlap detected", this, vehicle);
+
+                    // Check if there's 1 or more overlap
+                    if (vehicleOverlaps.length >= 1) {
+                        // Hide global overlap
+                        globalOverlap.overlap.hide()
+
+                        // Join global overlap with current overlap
+                        globalOverlap.feature = utils.union(overlap, globalOverlap.feature)
+                    } else {
+                        // Set current overlap as global overlap
+                        globalOverlap.feature = overlap;
+                    }
+
+                    // Activate new overlap
+                    const newGlobalOverlap = new Overlap(globalOverlap.feature);
+                    newGlobalOverlap.show();
+                    globalOverlap.overlap = newGlobalOverlap;
+
                     // Create overlap
                     const currentOverlap = new Overlap(overlap);
-                    currentOverlap.show();
-                    this.overlaps.push(currentOverlap);
 
                     // Store both vehicles and overlap 
                     vehicleOverlaps.push([vehicle, this, currentOverlap]);
                 }
             }
         });
-        console.log(vehicleOverlaps)
     }
 
     clearOverlaps() {
+        console.log("Overlaps: ", vehicleOverlaps)
+
+        // Hide global overlap
+        globalOverlap.overlap.hide();
+
+
+        // Clip global overlap by this poly 
+        const clip = utils.difference(globalOverlap.feature, this.polygon);
+
+        if (clip) {
+            globalOverlap.feature = clip;
+            const newOverlap = new Overlap(clip);
+            globalOverlap.overlap = newOverlap;
+            newOverlap.show();
+        }
+
         // Check if there's any active overlap
         for (let i = 0; i < vehicleOverlaps.length; i++) {
             if (vehicleOverlaps[i] && (vehicleOverlaps[i][0] == this || vehicleOverlaps[i][1] == this)) {
                 vehicleOverlaps[i][2].hide(); // Hide it
-                delete vehicleOverlaps[i]; // Remove it
+                vehicleOverlaps.splice(i, 1); // Remove it
             }
         }
+
+        if (vehicleOverlaps.length == 0) {
+            globalOverlap.overlap.hide();
+        }
+    }
+
+    updatePosition(coord: LatLng) {
+        this.position.lat = coord.lat;
+        this.position.lng = coord.lng;
+        this.marker.setLatLng(coord);
     }
 
     extract() {
         this.position.lng = this.position.lng + 0.0025;
-        this.marker.addToMap();
+        this.marker.addToMap(this.targetLayer);
     }
 }
