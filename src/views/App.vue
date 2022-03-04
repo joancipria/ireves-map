2015f7396d604529a3199248bef689941205475b
Joan Ciprià <joancipria@protonmail.com>
2022-03-04 11:01:05 +0100

    Add notification and export panel components

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <Notification></Notification>
   <NavBar
     @show-data-loader="showDataLoader"
     @show-settings="showSettings"
+    @show-export="showExportPanel"
   ></NavBar>
   <FileLoader :mode="fileLoaderMode" ref="fileLoader" @base-selected="onBaseSelected"></FileLoader>
   <Settings ref="settings"></Settings>
   <BaseDetails ref="baseDetails" :base="selectedBase"></BaseDetails>
+  <ExportPanel ref="exportPanel"></ExportPanel>
   <Leaflet-map></Leaflet-map>
 </template>
 
@@ -14,8 +17,10 @@ import { Options, Vue } from "vue-class-component";
 import LeafletMap from "@/components/LeafletMap.vue"; // @ is an alias to /src
 import FileLoader from "@/components/FileLoader.vue";
 import BaseDetails from "@/components/BaseDetails.vue";
+import ExportPanel from "@/components/ExportPanel.vue";
 import NavBar from "@/components/NavBar.vue";
 import Settings from "@/components/Settings.vue";
+import Notification from "@/components/Notification.vue";
 import Base from "@/core/Base";
 
 @Options({
@@ -23,8 +28,10 @@ import Base from "@/core/Base";
     LeafletMap,
     FileLoader,
     BaseDetails,
+    ExportPanel,
     NavBar,
     Settings,
+    Notification
   },
 })
 export default class App extends Vue {
@@ -35,15 +42,22 @@ export default class App extends Vue {
     baseDetails: BaseDetails;
     settings: Settings;
     fileLoader: FileLoader;
+    exportPanel: ExportPanel;
   };
 
   onBaseSelected(base: Base) {
     this.selectedBase = base;
     this.$refs.baseDetails.show();
+    this.$refs.exportPanel.hide();
   }
 
   showSettings() {
     this.$refs.settings.show();
+  }
+
+  showExportPanel (){
+    this.$refs.exportPanel.show();
+    this.$refs.baseDetails.hide();
   }
 
   showDataLoader() {
