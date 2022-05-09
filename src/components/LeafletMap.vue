1f9f1eb303876276b30e7e5ab772d26336f08dc1
Joan Ciprià <joancipria@protonmail.com>
2022-05-09 17:31:27 +0200

    Remove layer control

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -4,12 +4,12 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { Control, LatLng, Map, TileLayer } from "leaflet";
+import { LatLng, Map, TileLayer } from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 
 // Marker Cluster. See https://github.com/Leaflet/Leaflet.markercluster/issues/874 & https://stackoverflow.com/questions/69477915/leaflet-litelement-l-is-not-defined-in-plugin
-import { LayerGroup, DivIcon } from 'leaflet';
+import { LayerGroup, DivIcon } from "leaflet";
 import { MarkerClusterGroup } from "leaflet.markercluster/src";
 
 @Options({
@@ -26,20 +26,6 @@ export default class LeafletMap extends Vue {
   map!: Map;
 
   mounted() {
-    const baseLayers = {
-      Openstreet: new TileLayer(
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      ),
-      Grayscale: new TileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
-        { id: "mapbox/light-v9" }
-      ),
-      Streets: new TileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
-        { id: "mapbox/streets-v11" }
-      ),
-    };
-
     const bounds: any = [
       new LatLng(44.638612045930095, -13.782874547541086),
       new LatLng(33.37482804543294, 11.521634438365721),
@@ -49,23 +35,10 @@ export default class LeafletMap extends Vue {
       preferCanvas: true,
       zoomControl: true,
       minZoom: 6,
-      layers: [
-        baseLayers.Openstreet,
-        layers.vehiclesCluster,
-        layers.basesCluster
-      ],
+      layers: [layers.openstreet, layers.vehiclesCluster, layers.basesCluster],
       attributionControl: false,
       maxBounds: bounds, // Set the map's geographical boundaries.
     });
-
-    let overlayMaps = {
-      "Bases": layers.basesCluster,
-      "Ambulancias": layers.vehiclesCluster,
-    };
-
-    const layersControl = new Control.Layers(baseLayers, overlayMaps, {
-      position: "topleft",
-    }).addTo(this.map);
 
     this.map.zoomControl.setPosition("bottomleft");
     this.map.setView([39.47482213445976, -0.3747370894871375], 8);
@@ -76,10 +49,27 @@ export default class LeafletMap extends Vue {
 
 // Map layers
 export const layers = {
-    vehiclesCluster: new LayerGroup(),
-    basesCluster: new MarkerClusterGroup({ chunkedLoading: true, 	iconCreateFunction: function(cluster) {
-		return new DivIcon({ html: `<div>${cluster.getChildCount()}</div>`, className: "base-cluster-marker" });
-	} })
+  openstreet: new TileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  ),
+  grayscale: new TileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+    { id: "mapbox/light-v9" }
+  ),
+  streets: new TileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+    { id: "mapbox/streets-v11" }
+  ),
+  vehiclesCluster: new LayerGroup(),
+  basesCluster: new MarkerClusterGroup({
+    chunkedLoading: true,
+    iconCreateFunction: function (cluster) {
+      return new DivIcon({
+        html: `<div>${cluster.getChildCount()}</div>`,
+        className: "base-cluster-marker",
+      });
+    },
+  }),
 };
 
 // Map
