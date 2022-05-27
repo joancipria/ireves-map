deaee2afc87a3154bcbfdb2cf1f54438ea820e9a
Joan Ciprià <joancipria@protonmail.com>
2022-05-27 13:51:07 +0200

    Rewrite base logic

--- src/core/Base.ts
+++ src/core/Base.ts
@@ -4,6 +4,7 @@ import Vehicle from "./Vehicle";
 import { MapEntity } from "@/core/MapEntity"
 import { VehicleType } from "@/core/Vehicle"
 import { layers } from "@/components/LeafletMap.vue";
+import { dragging, vehicles } from "@/main";
 
 export enum BaseType {
     HOSPITAL = 'HOSPITAL',
@@ -20,7 +21,6 @@ export default class Base {
     address: string;
     vehicles: Vehicle[] = [];
     marker: LeafletMarker;
-    activeVehicles: any;
 
     constructor(name: string, type: BaseType, lat: number = 0, lng: number = 0, address: string, region: string) {
         this.name = name;
@@ -28,51 +28,104 @@ export default class Base {
         this.position = new LatLng(lat, lng);
         this.address = address;
         this.region = region;
-        this.marker = new LeafletMarker(MapEntity.BASE, this.position, false, '', true, layers.basesCluster);
-    }
 
-    extractVehicle(vehicleToExtract: Vehicle) {
-        const index = this.vehicles.findIndex((vehicle: Vehicle) => {
-            if (vehicle === vehicleToExtract) {
-                return true;
+        // Determine initial marker class
+        let markerClass = "";
+        if (vehicles.length <= 0) {
+            markerClass = "disabled-marker";
+        }
+
+        // Create marker
+        this.marker = new LeafletMarker(MapEntity.BASE, this.position, false, markerClass, true, layers.basesCluster, this.name);
+
+        /* Drag and drop vehicles system */
+        // On mouse over
+        this.marker.on('mouseover', () => {
+            // Set base flag
+            dragging.base = this;
+
+            // If user is dragging a vehicle
+            if (dragging.vehicle) {
+                // Increase base icon size (animated)
+                this.marker.getElement().classList.add("increased-marker");
+
+
+                // Set vehicle dragend listener
+                dragging.vehicle.marker.on('dragend', () => {
+                    if (dragging.base && dragging.base == this) {
+                        this.park(dragging.vehicle);
+                        this.toggleBase(true);
+                        dragging.vehicle = undefined;
+                        dragging.base = false;
+                    }
+                })
             }
         });
-        this.vehicles.splice(index, 1); // returns array of removed items
-        vehicleToExtract.extract();
-        this.refreshActiveVehicles();
-        this.showIsochrone();
+
+        this.marker.on('mouseout', () => {
+            this.marker.getElement().classList.remove("increased-marker");
+            dragging.base = false;
+        })
     }
 
-    showIsochrone() {
-        this.refreshActiveVehicles();
-        if (this.activeVehicles.samu) {
-            this.activeVehicles.samu.activate();
+    extractVehicle(vehicleID: string): void {
+        const targetIndex = this.vehicles.findIndex(vehicle => vehicle.id == vehicleID);
+        this.vehicles[targetIndex].show();
+
+        vehicles.push(this.vehicles[targetIndex])
+        this.vehicles.splice(targetIndex, 1);
+
+        if (this.vehicles.length <= 0) {
+            this.toggleBase(false);
         }
-        if (this.activeVehicles.svb) {
-            this.activeVehicles.svb.activate();
+    }
+
+    park(vehicle: Vehicle): void {
+        vehicle.updatePosition(this.position);
+        vehicle.marker.setLatLng(this.position);
+        vehicle.hide();
+        this.vehicles.push(vehicle);
+
+        this.toggleBase(true);
+
+    }
+
+    async showIsochrone() {
+        const vehicles = this.getActiveVehicles();
+        if (vehicles.samu) {
+            await vehicles.samu.activate();
+        }
+        if (vehicles.svb) {
+            await vehicles.svb.activate();
         }
     }
 
     async getPopulation() {
-        this.refreshActiveVehicles();
-        const data = { samu: null, svb: null }
-        if (this.activeVehicles.samu) {
-            data.samu = await this.activeVehicles.samu.getPopulation();
+        const vehicles = this.getActiveVehicles();
+        const data = { samu: 0, svb: 0 }
+        if (vehicles.samu) {
+            data.samu = await vehicles.samu.getPopulation();
         }
-        if (this.activeVehicles.svb) {
-            data.svb = await this.activeVehicles.svb.getPopulation();
+        if (vehicles.svb) {
+            data.svb = await vehicles.svb.getPopulation();
         }
         return data;
     }
 
-
-    refreshActiveVehicles() {
+    getActiveVehicles() {
         const samu = this.vehicles.find(vehicle => vehicle.type == VehicleType.SAMU);
         const svb = this.vehicles.find(vehicle => vehicle.type == VehicleType.SVB);
 
-        this.activeVehicles = {
-            samu: samu,
-            svb: svb
+        return { samu: samu, svb: svb };
+    }
+
+    toggleBase(enabled: boolean) {
+        const markerIcon = this.marker.getElement().getElementsByTagName('img')[0];
+
+        if (enabled) {
+            markerIcon.classList.remove("disabled-marker");
+        } else {
+            markerIcon.classList.add("disabled-marker");
         }
     }
 
