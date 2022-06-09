87ee6a6a2b8cccbaa238f43bf1ec73140172226d
Joan Ciprià <joancipria@protonmail.com>
2022-06-09 12:13:09 +0200

    Create models service

---
+++ src/services/models.service.ts
@@ -0,0 +1,15 @@
+import HttpClient from "./httpclient.service";
+import config from "@/config";
+
+class ModelsService extends HttpClient {
+
+    constructor() {
+        super(config.SERVER_URL);
+    }
+
+    getStaticModel(model: string) {
+        return super.get('/model/static', model);
+    }
+}
+
+export const modelsService = new ModelsService();
